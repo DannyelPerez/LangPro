53f59f5802d0c9117cc728cb5ea221cb801d43df
josuereus <barahona.josue@hotmail.com>
2016-06-16 11:04:54 -0600

    ng-repeat implemente in cards

--- dev/components/project/project.controller.js
+++ dev/components/project/project.controller.js
@@ -8,5 +8,17 @@
     function projectController($state, $mdSidenav, $scope) {
         var vm = this;
         vm.imagePath = 'https://material.angularjs.org/latest/img/washedout.png';
+        vm.imagePath2 = 'images/pc.png';
+
+        vm.cards = [{
+        	image:'https://material.angularjs.org/latest/img/washedout.png',
+        	title:'This is the title 1',
+        	desc:"This is the first card"
+        },
+        {
+        	image:'images/pc.png',
+        	title:'This is the title 2',
+        	desc:'This is the second card'
+        }];
     }
 })();
